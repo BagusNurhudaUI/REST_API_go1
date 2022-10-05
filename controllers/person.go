c5e69800e0d00905d851a67a2980e9f232f31607
Bagus <bagus.nurhuda@ui.ac.id>
2022-10-05 22:08:22 +0700

    third change

--- controllers/person.go
+++ controllers/person.go
@@ -28,3 +28,115 @@ func (idb *InDB) GetPerson(c *gin.Context) {
 	}
 	c.JSON(http.StatusOK, result)
 }
+
+func (db *InDB) GetPersons(c *gin.Context) {
+	var (
+		persons []structs.Person
+		result  gin.H
+	)
+
+	db.DB.Find(&persons)
+
+	if len(persons) <= 0 {
+		result = gin.H{
+			"result": nil,
+			"count":  0,
+		}
+	} else {
+		result = gin.H{
+			"result": persons,
+			"count":  len(persons),
+		}
+	}
+
+	c.JSON(http.StatusOK, result)
+}
+
+func (db *InDB) CreatePerson(c *gin.Context) {
+	var (
+		person structs.Person
+		result gin.H
+	)
+
+	firstName := c.PostForm("first_name")
+	lastName := c.PostForm("last_name")
+
+	person.FirstName = firstName
+	person.LastName = lastName
+
+	db.DB.Create(&person)
+
+	result = gin.H{
+		"result": person,
+	}
+
+	c.JSON(http.StatusOK, result)
+}
+
+func (db *InDB) UpdatePerson(c *gin.Context) {
+	id := c.Query("id")
+
+	firstName := c.PostForm("first_name")
+	lastName := c.PostForm("last_name")
+
+	var (
+		person    structs.Person
+		newPerson structs.Person
+		result    gin.H
+	)
+
+	err := db.DB.First(&person, id).Error
+	if err != nil {
+		result = gin.H{
+			"result": "Data not found",
+		}
+	}
+
+	newPerson.FirstName = firstName
+	newPerson.LastName = lastName
+
+	err = db.DB.Model(&person).Updates(newPerson).Error
+
+	if err != nil {
+		result = gin.H{
+			"result": "Update Failed",
+		}
+	} else {
+		result = gin.H{
+			"result": "Successfully updated data",
+		}
+	}
+
+	c.JSON(http.StatusOK, result)
+}
+
+func (db *InDB) DeletePerson(c *gin.Context) {
+	var (
+		person structs.Person
+		result gin.H
+	)
+
+	id := c.Param("id")
+
+	err := db.DB.First(&person, id).Error
+
+	if err != nil {
+		result = gin.H{
+			"result": "Data not found",
+		}
+	}
+
+	err = db.DB.Delete(&person).Error
+
+	if err != nil {
+		result = gin.H{
+			"result": "Delete failed",
+		}
+	} else {
+		result = gin.H{
+			"result": "Data deleted successfully",
+		}
+	}
+
+	c.JSON(http.StatusOK, result)
+}
